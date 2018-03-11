71cc4ff953884577335fdefab3a89c71ad0ef531
HildePols <atschpe@gmail.com>
2018-03-11 19:59:27 +0100

    Added DTO generator Adapter not loading because list is empty

--- app/src/main/java/com/example/android/bakeme/data/model/Recipe.java
+++ app/src/main/java/com/example/android/bakeme/data/model/Recipe.java
@@ -1,6 +1,5 @@
 package com.example.android.bakeme.data.model;
 
-import android.os.Parcel;
 import android.os.Parcelable;
 
 import com.google.gson.annotations.Expose;
@@ -8,90 +7,41 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+import retrofit2.Retrofit;
+
 /**
  * {@link Recipe} is an object holding the various infos provided by the API concerning all
  * aspects of the recipe in question. It is setup to using http://www.jsonschema2pojo.org/
- * to enable {@link retrofit2.Retrofit} to use it and implements {@link Parcelable} to enable
+ * to enable {@link Retrofit} to use it and implements {@link Parcelable} to enable
  * data persistence.
  */
-public class Recipe implements Parcelable {
+public class Recipe {
 
-    @SerializedName("id")
     @Expose
-    private int id;
-    @SerializedName("name")
-    @Expose
-    private String name;
-    @SerializedName("ingredients")
-    @Expose
-    private List<Ingredient> ingredients = null;
-    @SerializedName("steps")
-    @Expose
-    private List<Step> steps = null;
-    @SerializedName("servings")
-    @Expose
-    private int servings;
     @SerializedName("image")
-    @Expose
     private String image;
-    public final static Parcelable.Creator<Recipe> CREATOR = new Creator<Recipe>() {
+    @Expose
+    @SerializedName("servings")
+    private int servings;
+    @Expose
+    @SerializedName("steps")
+    private List<Steps> steps;
+    @Expose
+    @SerializedName("ingredients")
+    private List<Ingredients> ingredients;
+    @Expose
+    @SerializedName("name")
+    private String name;
+    @Expose
+    @SerializedName("id")
+    private int id;
 
-
-        @SuppressWarnings({
-                "unchecked"
-        })
-        public Recipe createFromParcel(Parcel in) {
-            return new Recipe(in);
-        }
-
-        public Recipe[] newArray(int size) {
-            return (new Recipe[size]);
-        }
-
-    };
-
-    protected Recipe(Parcel in) {
-        this.id = ((int) in.readValue((int.class.getClassLoader())));
-        this.name = ((String) in.readValue((String.class.getClassLoader())));
-        in.readList(this.ingredients, (Ingredient.class.getClassLoader()));
-        in.readList(this.steps, (Step.class.getClassLoader()));
-        this.servings = ((int) in.readValue((int.class.getClassLoader())));
-        this.image = ((String) in.readValue((String.class.getClassLoader())));
+    public String getImage() {
+        return image;
     }
 
-    public Recipe() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(List<Ingredient> ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public List<Step> getSteps() {
-        return steps;
-    }
-
-    public void setSteps(List<Step> steps) {
-        this.steps = steps;
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public int getServings() {
@@ -102,36 +52,129 @@ public class Recipe implements Parcelable {
         this.servings = servings;
     }
 
-    public String getImage() {
-        return image;
+    public List<Steps> getSteps() {
+        return steps;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setSteps(List<Steps> steps) {
+        this.steps = steps;
     }
 
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeValue(id);
-        dest.writeValue(name);
-        dest.writeList(ingredients);
-        dest.writeList(steps);
-        dest.writeValue(servings);
-        dest.writeValue(image);
+    public List<Ingredients> getIngredients() {
+        return ingredients;
     }
 
-    public int describeContents() {
-        return 0;
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
     }
 
-//    public static List<Recipe> getRecipeList() {
-//        return recipeList;
-//    }
-//
-//    public void setRecipeList(List<Recipe> recipeList) {
-//        this.recipeList = recipeList;
-//    }
-//
-//    @SerializedName("recipeList")
-//    private static List<Recipe> recipeList;
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static class Steps {
+        @Expose
+        @SerializedName("thumbnailURL")
+        private String thumbnailurl;
+        @Expose
+        @SerializedName("videoURL")
+        private String videourl;
+        @Expose
+        @SerializedName("description")
+        private String description;
+        @Expose
+        @SerializedName("shortDescription")
+        private String shortdescription;
+        @Expose
+        @SerializedName("id")
+        private int id;
+
+        public String getThumbnailurl() {
+            return thumbnailurl;
+        }
+
+        public void setThumbnailurl(String thumbnailurl) {
+            this.thumbnailurl = thumbnailurl;
+        }
+
+        public String getVideourl() {
+            return videourl;
+        }
+
+        public void setVideourl(String videourl) {
+            this.videourl = videourl;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getShortdescription() {
+            return shortdescription;
+        }
+
+        public void setShortdescription(String shortdescription) {
+            this.shortdescription = shortdescription;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
+    public static class Ingredients {
+        @Expose
+        @SerializedName("ingredient")
+        private String ingredient;
+        @Expose
+        @SerializedName("measure")
+        private String measure;
+        @Expose
+        @SerializedName("quantity")
+        private int quantity;
+
+        public String getIngredient() {
+            return ingredient;
+        }
+
+        public void setIngredient(String ingredient) {
+            this.ingredient = ingredient;
+        }
+
+        public String getMeasure() {
+            return measure;
+        }
+
+        public void setMeasure(String measure) {
+            this.measure = measure;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
 }
